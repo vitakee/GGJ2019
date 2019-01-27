f756746f055efb821298ce44b09e9cc0d007d1ec
Antoine COLIN <antoine35.colin@gmail.com>
2019-01-27 12:52:46 +0700

    Buddy Sound

---
+++ Assets/Script/Buddy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buddy : MonoBehaviour
+{
+	AudioSource audioSource;
+	public AudioClip finishSound;
+	public AudioClip painSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+		audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void DestroyBuddy()
+	{
+		audioSource.PlayOneShot(finishSound);
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		audioSource.PlayOneShot(painSound, 0.5f);
+	}
+}
--- Assets/Script/Finish.cs
+++ Assets/Script/Finish.cs
@@ -44,8 +44,10 @@ public class Finish : MonoBehaviour
                 Debug.LogWarning("GREEN buddy here");
                 numberBuddy[2]--;
 				remainingOnes--;
+				
 			}
             Debug.LogWarning("Checking for victory");
+			other.gameObject.GetComponent<Buddy>().DestroyBuddy();
             CheckForVictory();
 		}
 	}
@@ -53,18 +55,11 @@ public class Finish : MonoBehaviour
 
 	public void CheckForVictory()
 	{
-<<<<<<< HEAD
-        if (remainingOnes <= 0)
-        {
-            Debug.LogWarning("Loading lvl2");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-=======
+
 		if (remainingOnes <= 0)
 		{
 			Debug.Log("Load new level");
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 		}
->>>>>>> ed40af55f9de6e7d32577b276b9dd3a6544c164f
 	}
 }
